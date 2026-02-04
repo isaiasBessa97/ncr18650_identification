b06f8772d6b19b256a77c30aec2a95a77939e5c1
Isaias Bessa <isaias.97.ib@gmail.com>
2026-02-04 11:01:55 -0300

    Verifying first datas of BID003

--- DOC_000_verify.m
+++ DOC_000_verify.m
@@ -1,2 +1,15 @@
 close all, clear all, clc
-meas = readtable("dataset-thermal\BID003\BID003_CDch005.0_10112025.txt")
+meas = readtable("dataset-thermal\BID002\BID002_CDch005.0_19022025.txt");
+time = meas.time;
+volt = meas.voltage;
+curr = meas.current;
+
+figure()
+plot(time,curr)
+xlabel("Time (s)")
+ylabel("Current (A)")
+
+figure()
+plot(time,volt)
+xlabel("Time (s)")
+ylabel("Voltage (V)")
