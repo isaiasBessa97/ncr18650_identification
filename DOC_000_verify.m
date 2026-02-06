ca641a3753620dde9e5fa2ed4ff518d7e7fc2391
Isaias Bessa <isaias.97.ib@gmail.com>
2026-02-06 12:21:07 -0300

    Profiles verified

--- DOC_000_verify.m
+++ DOC_000_verify.m
@@ -1,8 +1,10 @@
 close all, clear all, clc
-meas = readtable("dataset-thermal\BID002\BID002_CDch005.0_19022025.txt");
+meas = readtable("dataset-thermal\BID003\BID003_CDch200.0_04022026.txt");
 time = meas.time;
 volt = meas.voltage;
 curr = meas.current;
+temp_s = meas.surface_temperature_plus;
+temp_a = meas.ambient_temperature;
 
 figure()
 plot(time,curr)
@@ -13,3 +15,13 @@ figure()
 plot(time,volt)
 xlabel("Time (s)")
 ylabel("Voltage (V)")
+
+figure()
+plot(time,temp_s)
+xlabel("Time (s)")
+ylabel("Surface temperature (ºC)")
+
+figure()
+plot(time,temp_a)
+xlabel("Time (s)")
+ylabel("Ambient temperature (ºC)")
