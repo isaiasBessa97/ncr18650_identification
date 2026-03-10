7d49ce3d86a8cf7d0c1647791c90910b4572581d
CaptainBidou <tomas.salvado_robalo@ig2i.centralelille.fr>
2026-03-10 17:05:35 -0300

    Constant profiles BID005

--- DOC_000_verify.m
+++ DOC_000_verify.m
@@ -1,5 +1,5 @@
 close all, clear all, clc
-meas = readtable("dataset-thermal\BID004\BID004_RSDch_03032026.txt");
+meas = readtable("dataset-thermal\BID005\BID005_CDch200.0_06032026.txt");
 time = meas.time;
 volt = meas.voltage;
 curr = meas.current;
