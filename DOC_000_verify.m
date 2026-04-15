1d3a82b5db4752da31f143810c5786f82cf8afc4
Isaias Bessa <isaias.97.ib@gmail.com>
2026-04-15 06:29:53 -0300

    nothing

--- DOC_000_verify.m
+++ DOC_000_verify.m
@@ -1,5 +1,5 @@
 close all, clear all, clc
-meas = readtable("dataset-thermal\BID008\BID008_RSDch_02042026.txt");
+meas = readtable("dataset-thermal\BID008\BID008_CDch200.0_27032026.txt");
 time = meas.time;
 volt = meas.voltage;
 curr = meas.current;
